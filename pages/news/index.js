eb98175c7ae34655543d022d4c2c5669dc2899be
munkhuu1126 <munkh1151@gmail.com>
2022-04-01 09:22:28 +0800

    uninstalled 2 carousel packages that are not needed

--- pages/news/index.js
+++ pages/news/index.js
@@ -35,7 +35,7 @@ export default function news({ data }) {
                 <title>News</title>
             </Head>
             <Header latestNews={data.data[0]}  />
-            <div className="container mx-auto lg:px-[5rem] xl:px-[8rem] 2xl:px-[10rem] gap-y-5 px-20 md:px-24 lg:gap-x-6 gap-x-5 grid md:grid-cols-2 lg:grid-cols-3 my-10 ">
+            <div className="container mx-auto lg:px-[5rem] xl:px-[8rem] 2xl:px-[10rem] gap-y-5 px-10 md:px-24 lg:gap-x-6 gap-x-5 grid md:grid-cols-2 lg:grid-cols-3 my-10 ">
                 {
                     newsContent
                 }
