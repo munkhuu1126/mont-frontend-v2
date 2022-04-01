568cf9c689f073bfc765fb2ceb3da0c138db2e5d
munkhuu1126 <munkh1151@gmail.com>
2022-04-01 12:26:25 +0800

    fixed padding for intro section

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -7,7 +7,7 @@ export default function Intro() {
   const {t}=useTranslation()
   return (
     <div className="text-white bg-center">
-      <div className="container mx-auto lg:py-44 px-16">
+      <div className="container mx-auto py-44 px-16">
         <h1 className='text-[3.25rem] font-bold mb-3'>{t('first_stable_coin')}</h1>
         <h2 className='text-[#eeeeee] text-2xl'>{t('first_stable_coin_text')}</h2>
       </div>
