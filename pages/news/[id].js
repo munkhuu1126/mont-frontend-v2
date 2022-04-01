0f112e47c7558eaf0ffe476f361ce98ff9ce6a30
munkhuu1126 <munkh1151@gmail.com>
2022-04-01 11:29:12 +0800

    fixed small syntax error

--- pages/news/[id].js
+++ pages/news/[id].js
@@ -43,7 +43,7 @@ export async function getStaticPaths(){
 }
 
 export async function getStaticProps({params}){
-  const res = await axios.get(`https://news.stablecoin.mn/Items/News${params.id}`)
+  const res = await axios.get(`https://news.stablecoin.mn/Items/News/${params.id}`)
   const data = await res.data
 
   return {
