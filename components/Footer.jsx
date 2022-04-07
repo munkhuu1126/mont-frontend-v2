145ce551d3b1233e748aed4540750914660ea077
munkhuu1126 <munkh1151@gmail.com>
2022-04-07 18:01:20 +0800

    changed marketcap, fixed footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -12,7 +12,7 @@ export default function Footer() {
             <div className='container mx-auto px-10 py-16 justify-between md:flex'>
 
 
-                <div className="md:space-y-8 space-y-4 mb-10 md:w-1/3 md:block grid grid-rows-3 justify-center">
+                <div className=" mb-10 md:w-1/3 md:block grid grid-rows-3 justify-center">
                     <div className="flex items-center justify-center md:justify-start space-x-2">
                         <div className="">
                             <Image src={mont} width='50px' height='50px' alt="" />
@@ -20,7 +20,7 @@ export default function Footer() {
                         </div>
                         <p className="text-white font-semibold text-3xl">MONT</p>
                     </div>
-                    <p>Mongolia&apos;s First StableCoin</p>
+                    <p className="mb-8 mt-2">Mongolia&apos;s First StableCoin</p>
                     <div className="text-white justify-between md:justify-start flex space-x-4 text-3xl">
                         <BsTwitter />
                         <BsFacebook />
@@ -32,28 +32,28 @@ export default function Footer() {
 
                 <div className="grid grid-cols-3 justify-between md:w-2/3">
                     <ul className="text-sm">
-                        <h1 className="text-lg mb-3">Products</h1>
-                        <li>Benefit</li>
-                        <li>Service</li>
-                        <li>Project Team</li>
-                        <li>Partners</li>
-                        <li>FAQ</li>
+                        <h1 className="text-lg font-semibold mb-3">Products</h1>
+                        <li className="my-1">Benefit</li>
+                        <li className="my-1">Service</li>
+                        <li className="my-1">Project Team</li>
+                        <li className="my-1">Partners</li>
+                        <li className="my-1">FAQ</li>
                     </ul>
 
                     <ul className="text-sm">
-                        <h1 className="text-lg mb-3">Support</h1>
-                        <li>Support</li>
+                        <h1 className="text-lg font-semibold mb-3">Support</h1>
+                        <li className="my-1">Support</li>
                     </ul>
 
                     <ul className="text-sm">
-                        <h1 className="text-lg mb-3">Service</h1>
-                        <li>Help Center</li>
-                        <li>Referrals</li>
+                        <h1 className="text-lg font-semibold mb-3">Service</h1>
+                        <li className="my-1">Help Center</li>
+                        <li className="my-1">Referrals</li>
                     </ul>
                 </div>
             </div>
             <div className="md:flex space-y-10 mx-auto container items-center justify-between px-10 pb-10">
-                
+
                 <div className="flex items-center space-x-4">
                     <p>Audited by </p>
                     <div className="w-[180px] h-auto">
--- components/MarketCap.jsx
+++ components/MarketCap.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import mont from '../img/mont.svg'
+import mont from '../img/mont-logo.png'
 import Image from 'next/image'
 import Link from 'next/link'
 import Swal from 'sweetalert2'
-import {BiCopy} from 'react-icons/bi'
+import { BiCopy } from 'react-icons/bi'
 
 export default function MarketCap() {
-    const copy = ()=>{
+    const copy = () => {
         navigator.clipboard.writeText('0x2D279FDECdf7f5705F5ff0bD80F8D9a305Ea87F4')
         Swal.fire({
             position: 'center',
@@ -14,48 +14,25 @@ export default function MarketCap() {
             showConfirmButton: false,
             timer: 1500
         })
-        
+
     }
     return (
-        <div className="md:flex container xl:px-10 px-4 mx-auto">
-            <div className="bg-blue-500  rounded-tl-xl md:rounded-bl-xl rounded-tr-xl md:rounded-tr-none  w-full md:flex justify-between items-center p-5 lg:p-10 mt-20">
-                <div className="md:flex">
-                    <div className="-translate-x-4 md:translate-x-0 items-center flex md:mb-0 mb-2 ">
-                        <Image src={mont} alt="" />
-                    </div>
-                    <div className="md:ml-10 ">
-                        <h1 className="text-[#f5f0f0]">MarketCap</h1>
-                        <p className="text-white lg:text-2xl text-md mt-1">1,458,657 MNT</p>
-                        <h1 className="text-[#f5f0f0] mt-4">Token Address</h1>
-                        <button 
-                        className="text-white lg:text-2xl md:text-md flex items-center text-sm lg:no-underline underline lg:hover:underline mt-1" 
-                        onClick={() => copy()}
-                        >
-                            0x2D279FDECdf7f5705F5ff0bD80F8D9a305Ea87F4 <BiCopy/>
-                        </button>
-                        
-                    </div>
-                </div>
+        <div className="xl:px-10 px-4 relative flex items-center my-20 container mx-auto">
+            <div className="absolute md:w-[250px] md:h-[250px] w-[120px] h-[120px]">
+                <Image src={mont} alt="" width='250' height='250'></Image>
             </div>
-            <div className="bg-blue-700 md:rounded-tr-xl rounded-br-xl rounded-bl-xl md:rounded-bl-none flex items-center p-5 lg:p-10 md:mt-20">
-                <div className="md:mr-10 text-white">
-                    <div className="flex mb-2">
-                        <a href="https://bscscan.com/token/0x2D279FDECdf7f5705F5ff0bD80F8D9a305Ea87F4"
-                            className="mr-2 hover:text-blue-300 transition duration-100 ease-in">
-                            BSCScan
-                        </a>
-                        <i className="bi bi-box-arrow-up-right"></i>
+            <div className="container mx-auto lg:pl-20 md:pl-24 pl-10 ">
+                <div className="bg-white shadow-lg overflow-hidden rounded-xl md:pl-48 pl-24 md:py-7 py-5">
+                    <h1 className='text-gray-400 text-xs md:text-base md:mb-2 mb-1'>Market Cap</h1>
+                    <p className="md:text-5xl text-xl md:mb-5 mb-2 text-blue-500">1,423,932₮</p>
+                    <div className="lg:flex md:space-x-3">
+                        <h1 className="md:text-base text-xs text-gray-400">Token Address</h1>
+                        <div className="flex overflow-hidden cursor-pointer hover:text-blue-500 transition underline duration-200 ease-in" onClick={() => { copy() }}>
+                            <p className="md:block hidden">0x2D279FDECdf7f5705F5ff0bD80F8D9a305Ea87F4</p>
+                            <p className="md:hidden block">Click to Copy the Address</p>
+                            <BiCopy />
+                        </div>
                     </div>
-                    <div className="flex mb-2">
-                        <a href="#" className="mr-2 hover:text-blue-300 transition duration-100 ease-in">Github </a>
-                        <i className="bi bi-box-arrow-up-right"></i>
-                    </div>
-                    <div className="flex">
-                        <a href="https://krypto.mn/currencies/mongol-tether" className="mr-2 hover:text-blue-300 transition duration-100 ease-in">Krypto.mn </a>
-                        <i className="bi bi-box-arrow-up-right"></i>
-
-                    </div>
-
                 </div>
             </div>
         </div>
