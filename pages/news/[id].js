caa1106c7aad6b93b070099ddd99302235fda90d
munkhuu1126 <munkh1151@gmail.com>
2022-04-14 12:28:01 +0800

    added "back to news" button

--- pages/news/[id].js
+++ pages/news/[id].js
@@ -3,8 +3,11 @@ import axios from 'axios'
 import parser from 'html-react-parser'
 import Image from 'next/image'
 import Head from 'next/head'
+import { BsArrowLeft } from 'react-icons/bs'
+import { useRouter } from 'next/router'
 
 export default function Post({ data }) {
+  const router = useRouter()
   const currentContent = data.data
   function GraphCMSImageLoader({ src }) {
 
@@ -14,7 +17,12 @@ export default function Post({ data }) {
     <div className="container font-rubik mx-auto lg:px-0 px-8 prose prose-img:rounded-xl prose-p:text-lg hover:prose-a:text-red-500 prose-slate my-10">
       <Head>
         <title>{currentContent.title}</title>
+        <link rel="icon" href="/mont-logo.ico"/>
       </Head>
+      <button className="flex hover:-translate-x-5  group transition duration-200 hover:text-blue-500 ease-in space-x-2 my-2 items-center" onClick={()=>{router.push('/news')}}>
+        <BsArrowLeft/>
+        <span className="group-hover:border-blue-500 border-b">Бусад мэдээг унших</span> 
+      </button>
       <h1 className="font-bold break-words text-4xl">{currentContent.title}</h1>
       <div className="relative">
         <Image loader={GraphCMSImageLoader} src={currentContent.main_image} height="100%" width='100%' layout="responsive" objectFit='contain' alt='' className="rounded-lg" />
